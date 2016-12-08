df81cbb0479eba06a6aa548a405072f803449207
Helge Fredheim <helge.fredheim@nav.no>
2016-12-08 14:13:43 +0100

    Diverse visuelle tweaks etter gjennomgang med designer

--- web/src/main/java/moteoversiktfssfront/Dekorator.java
+++ web/src/main/java/moteoversiktfssfront/Dekorator.java
@@ -11,7 +11,7 @@ import static java.util.Arrays.asList;
 
 public class Dekorator extends DecoratorFilter {
     private static final String FRAGMENTS_URL = "/internarbeidsflatedecorator/index.html";
-    private static final String APPLICATION_NAME = "Møteoversikt";
+    private static final String APPLICATION_NAME = "Oversikt dialogmøter";
     private static final List<String> NO_DECORATOR_PATTERNS = new ArrayList<>(asList(".*/img/.*", ".*/css/.*", ".*/js/.*", ".*/font/.*", ".*selftest.*"));
     private static final List<String> FRAGMENT_NAMES = new ArrayList<>(asList("header-withmenu", "styles"));
 
