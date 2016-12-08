36917ec3a435a8ceb0a4bfffd78dc5f5c822856f
Helge Fredheim <helge.fredheim@nav.no>
2016-12-08 13:19:51 +0100

    La til filtrering

--- web/src/frontend/js/components/Moter.js
+++ web/src/frontend/js/components/Moter.js
@@ -1,9 +1,9 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { getDatoFraZulu } from '../utils/index';
 
 const statuser = {
     AVBRUTT: 'Avbrutt',
-    BEKREFTET: 'Bekreftet',
+    BEKREFTET: 'Møtetidspunkt bekreftet',
     OPPRETTET: 'Ikke svart',
     SVAR_MOTTATT: 'Svar mottatt',
 };
@@ -63,50 +63,105 @@ Mote.propTypes = {
     bruker: PropTypes.object,
 };
 
-const Moteoversikt = ({ moter }) => {
-    return (<div className="moteoversikt">
-            <h3 className="moteoversikt__meta">Viser {moter.length} {moter.length === 1 ? 'møte' : 'møter'}</h3>
-            <table className="motetabell">
-            <thead>
-                <tr>
-                    <th>F.nr</th>
-                    <th>Navn</th>
-                    <th>Nærmeste leder</th>
-                    <th>Sendt dato</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                {moter.sort((a, b) => {
-                    if (a.opprettetTidspunkt > b.opprettetTidspunkt) {
-                        return -1;
-                    }
-                    if (a.opprettetTidspunkt < b.opprettetTidspunkt) {
-                        return 1;
-                    }
-                    return 0;
-                }).map((mote, index) => {
-                    const bruker = mote.deltakere.filter((deltaker) => {
-                        return deltaker.type.toUpperCase() === 'BRUKER';
-                    })[0];
-                    const leder = mote.deltakere.filter((deltaker) => {
-                        return deltaker.type.toUpperCase() === 'ARBEIDSGIVER';
-                    })[0];
-                    return <Mote key={index} {...mote} leder={leder} bruker={bruker} />;
-                })}
-            </tbody>
-        </table>
+class Moteoversikt extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filter: 'alle',
+        };
+    }
+
+    getStatuser(moter) {
+        const alleStatuser = moter.map((mote) => {
+            return mote.status;
+        });
+        return [...new Set(alleStatuser)];
+    }
+
+    getFiltrerteMoter() {
+        const { moter } = this.props;
+        const filter = this.state.filter;
+        if (filter === 'alle') {
+            return moter;
+        }
+        return moter.filter((mote) => {
+            return mote.status === filter;
+        });
+    }
+
+    setStatus(status) {
+        this.setState({
+            filter: status,
+        });
+    }
+
+    render() {
+        const filtrerteMoter = this.getFiltrerteMoter();
+        const { moter } = this.props;
+        return (<div>
+            <div className="verktoylinje">
+                <div className="verktoylinje__verktoy">
+                    <label htmlFor="moteoversikt-filtrer">Filtrer på status</label>
+                    <div className="selectContainer">
+                        <select id="moteoversikt-filtrer" onChange={(e) => {
+                            this.setStatus(e.currentTarget.value);
+                        }}>
+                            <option value="alle">Vis alle</option>
+                            {
+                                this.getStatuser(moter).map((status, index) => {
+                                    return <option key={index} value={status}>{statuser[status]}</option>;
+                                })
+                            }
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className="moteoversikt">
+                <h3 className="moteoversikt__meta">Viser {filtrerteMoter.length} {filtrerteMoter.length === 1 ? 'møte' : 'møter'}</h3>
+                <table className="motetabell">
+                <thead>
+                    <tr>
+                        <th>F.nr</th>
+                        <th>Navn</th>
+                        <th>Nærmeste leder</th>
+                        <th>Sendt dato</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filtrerteMoter.sort((a, b) => {
+                        if (a.opprettetTidspunkt > b.opprettetTidspunkt) {
+                            return -1;
+                        }
+                        if (a.opprettetTidspunkt < b.opprettetTidspunkt) {
+                            return 1;
+                        }
+                        return 0;
+                    }).map((mote, index) => {
+                        const bruker = mote.deltakere.filter((deltaker) => {
+                            return deltaker.type.toUpperCase() === 'BRUKER';
+                        })[0];
+                        const leder = mote.deltakere.filter((deltaker) => {
+                            return deltaker.type.toUpperCase() === 'ARBEIDSGIVER';
+                        })[0];
+                        return <Mote key={index} {...mote} leder={leder} bruker={bruker} />;
+                    })}
+                </tbody>
+            </table>
+        </div>
     </div>);
-};
+    }
+}
 
 Moteoversikt.propTypes = {
     moter: PropTypes.array,
 };
 
-const Moter = ({ veileder, moter }) => {
+const Moter = ({ moter, veileder }) => {
     const moterMedStatus = moter.map(setMoteStatus).filter((mote) => {
         return mote.status !== 'AVBRUTT';
     });
+
     return (<div>
         <header className="veileder">
             <h2 className="veileder__navn">{veileder.navn} ({moterMedStatus.length})</h2>
@@ -117,7 +172,7 @@ const Moter = ({ veileder, moter }) => {
             </div>)
         }
         {
-            moterMedStatus.length > 0 && <Moteoversikt moter={moterMedStatus} />
+            moterMedStatus.length > 0 && <Moteoversikt moter={moter} />
         }
     </div>);
 };
