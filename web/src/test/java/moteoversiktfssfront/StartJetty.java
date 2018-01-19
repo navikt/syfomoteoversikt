4abd8fa5bbb14d015c0d85fd82fdc4010bfa5583
Andreas Lund <andreas.lund@nav.no>
2018-01-19 09:33:25 +0100

    Bytter openamagent

--- web/src/test/java/moteoversiktfssfront/StartJetty.java
+++ web/src/test/java/moteoversiktfssfront/StartJetty.java
@@ -1,28 +1,18 @@
 package moteoversiktfssfront;
 
-import no.nav.brukerdialog.security.context.InternbrukerSubjectHandler;
+import no.nav.brukerdialog.security.context.CustomizableSubjectHandler;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
-import org.apache.geronimo.components.jaspi.AuthConfigFactoryImpl;
-
-import javax.security.auth.message.config.AuthConfigFactory;
-import java.security.Security;
 
 import static java.lang.System.setProperty;
-import static no.nav.brukerdialog.security.context.InternbrukerSubjectHandler.setServicebruker;
-import static no.nav.brukerdialog.security.context.InternbrukerSubjectHandler.setVeilederIdent;
-import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
-import static no.nav.modig.lang.collections.RunnableUtils.first;
-import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
+import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.*;
 
 public class StartJetty {
     private static final int PORT = 8290;
 
     public static void main(String[] args) throws Exception {
-        Security.setProperty(AuthConfigFactory.DEFAULT_FACTORY_SECURITY_PROPERTY, AuthConfigFactoryImpl.class.getCanonicalName());
-        setVeilederIdent("Z990572");
-        setServicebruker("srvmoteoversiktfront");
-        setProperty("no.nav.modig.core.context.subjectHandlerImplementationClass", InternbrukerSubjectHandler.class.getName());
+        setProperty("no.nav.brukerdialog.security.context.subjectHandlerImplementationClass", CustomizableSubjectHandler.class.getName());
+
         Jetty jetty = usingWar()
                 .at("/moteoversikt")
                 .overrideWebXml()
