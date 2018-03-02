5a422a4283c74afbf5c887369d0064ed92c239f8
Andreas Lund <andreas.lund@nav.no>
2018-03-02 15:28:28 +0100

    SYFOUTV-1723: Løfter applikasjonen til NAIS

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,27 @@
+import no.nav.sbl.dialogarena.common.jetty.Jetty;
+
+import static java.lang.System.getProperty;
+import static java.lang.System.setProperty;
+import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
+import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.first;
+import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.gotKeypress;
+import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.waitFor;
+import static no.nav.sbl.dialogarena.test.SystemProperties.setFrom;
+import static no.nav.testconfig.ApiAppTest.setupTestContext;
+
+public class MainTest {
+
+    public static void main(String[] args) throws Exception {
+        setupTestContext();
+        setFrom("test.properties");
+        setProperty("OIDC_REDIRECT_URL", getProperty("VEILARBLOGIN_REDIRECT_URL_URL"));
+        Jetty jetty = usingWar()
+                .at("moteoversikt")
+                .port(8290)
+                .overrideWebXml()
+                .disableAnnotationScanning()
+                .buildJetty();
+        jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
+    }
+
+}
