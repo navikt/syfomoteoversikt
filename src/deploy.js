d7433da840dd5339a7aafc884722170233aa9c77
June Henriksen <june.henriksen2@nav.no>
2019-11-26 09:40:55 +0100

    Update deploy script

--- src/deploy.js
+++ src/deploy.js
@@ -1,27 +1,23 @@
-var child_process = require('child_process');
-var fs = require("fs");
+const childProcess = require('child_process');
+const fs = require('fs');
 
-var bundle = function() {
-	child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
-		console.log('stdout: ' + stdout);
-		console.log('stderr: ' + stderr);
-		if (error !== null) {
-			console.log('exec error: ' + error);
-		}
-	});
-}
+const timestamp = Date.now().toString();
 
-fs.exists("../main/webapp/js", function(exists) {
-	if(!exists) {
-		console.log("JS-mappe finnes ikke, og vi lager den")
-		// Lager JS-mappe hvis den ikke finnes
-		fs.mkdir("../main/webapp/js", function() {
-			console.log("JS-mappe laget")
-			// Når det er gjort, utfører vi bundling
-			bundle(); 
-		})
+fs.writeFile('./settings.json', JSON.stringify({
+	timestamp,
+	isProd: true,
+}), (error) => {
+	if (error) {
+		console.log('Feil ved lagring av settings.');
 	} else {
-		console.log("JS-mappe finnes, og vi bundler direkte")
-		bundle(); 
+		console.log('Settings lagret til ./settings.json');
+	}
+});
+
+childProcess.exec('webpack -p --config webpack.production.config.js', (error, stdout, stderr) => {
+	console.log('stdout: ' + stdout);
+	console.log('stderr: ' + stderr);
+	if (error !== null) {
+		console.log('exec error: ' + error);
 	}
 });
