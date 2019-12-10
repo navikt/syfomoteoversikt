e497d53bffb109a8710cc3f8439dc8d968475708
June Henriksen <june.henriksen2@nav.no>
2019-12-10 15:19:01 +0100

    [Kaizen] Fix lint error

--- src/js/utils/lenkeUtil.js
+++ src/js/utils/lenkeUtil.js
@@ -3,5 +3,5 @@ import { fullAppAdeoUrl } from './miljoUtil';
 export const syfomodiapersonMoterUrl = (fnr) => {
     const path = `/sykefravaer/${fnr}/mote`;
     return fullAppAdeoUrl(path);
-    //return fullNaisUrlDefault('syfomodiaperson', path);
+    // return fullNaisUrlDefault('syfomodiaperson', path);
 };
