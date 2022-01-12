dd2789219f4f5f38c83d4a4b0b4561516cb2c7eb
Erik Jansen <erik.gunnar.jansen@nav.no>
2022-01-12 13:23:44 +0100

    Simplify webpack setup for production and remove mini-css-extract-plugin (#71)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,43 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const autoprefixer = require("autoprefixer");
 
 module.exports = merge(common, {
   mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: false,
-              url: false,
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [autoprefixer],
-              },
-            },
-          },
-          {
-            loader: "less-loader",
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "styles.css",
-    }),
-  ],
 });
