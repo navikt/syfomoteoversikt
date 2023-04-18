e40fec16ea59cffb10ff6d7c699fb0a0639a7487
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2023-04-18 08:26:57 +0200

    Upgrade dev deps, fix lint and missing env var (#141)

--- src/data/modiacontext/useAktivBruker.ts
+++ src/data/modiacontext/useAktivBruker.ts
@@ -5,9 +5,10 @@ import { MODIACONTEXTHOLDER_ROOT } from "@/utils/apiUrlUtil";
 const NY_AKTIV_BRUKER = "NY_AKTIV_BRUKER";
 
 export const useAktivBruker = () =>
-  useMutation((fnr: string) =>
-    post(`${MODIACONTEXTHOLDER_ROOT}/context`, {
-      verdi: fnr,
-      eventType: NY_AKTIV_BRUKER,
-    })
-  );
+  useMutation({
+    mutationFn: (fnr: string) =>
+      post(`${MODIACONTEXTHOLDER_ROOT}/context`, {
+        verdi: fnr,
+        eventType: NY_AKTIV_BRUKER,
+      }),
+  });
