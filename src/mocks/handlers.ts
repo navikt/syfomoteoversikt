c45ae2b4231a9e8e5cabb2959ff12adcd1b3de01
eirikdahlen <dahleneirik@gmail.com>
2026-03-30 14:30:06 +0200

    IS: Handle websocket in mock

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,14 @@
-import { HttpHandler } from "msw";
+import { HttpHandler, WebSocketHandler, ws } from "msw";
 import { mockDialogmoter } from "@/mocks/mockDialogmoter";
 import { mockSyfoveileder } from "@/mocks/mockSyfoveileder";
 import mockEreg from "@/mocks/mockEreg";
 import { mockModiacontextholder } from "@/mocks/mockModiacontextholder";
 import mockSyfoperson from "@/mocks/mockSyfoperson";
 
-const handlers: HttpHandler[] = [
+const handlers: Array<HttpHandler | WebSocketHandler> = [
+  ws.link("ws://localhost:4000/*").addEventListener("connection", () => {
+    // Silently ignore WebSocket connections to Internflatedecorator in local development
+  }),
   mockEreg,
   mockSyfoperson,
   ...mockModiacontextholder,
