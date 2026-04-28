7da5f41d8291bbd8aad34cf026e88b1cbe005505
Peter Rydberg <peter.rydberg@bekk.no>
2026-04-28 15:28:43 +0200

    IS-3891: Webpack to Vite

--- webpack.prod.js
+++
@@ -1,8 +0,0 @@
-import { merge } from "webpack-merge";
-import common from "./webpack.common.js";
-
-const productionConfig = {
-  mode: "production",
-};
-
-export default merge(common, productionConfig);
